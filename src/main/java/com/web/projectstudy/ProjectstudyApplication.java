ad44f3fd99128357b370bf85aa2883f6df8c23cc
sseungki98 <79951703+sseungki98@users.noreply.github.com>
2022-02-02 22:49:36 +0900

    Initial commit

---
+++ src/main/java/com/web/projectstudy/ProjectstudyApplication.java
@@ -0,0 +1,13 @@
+package com.web.projectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/projectstudy/ProjectstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.projectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
